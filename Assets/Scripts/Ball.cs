fc08ecaebf6e4c045888a805221d74560a83a4c8
yudhasapp <90319545+yudhasapp@users.noreply.github.com>
2022-06-08 23:14:19 +0700

    Base gameplay

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    public Vector2 speed;
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.velocity = speed;
+    }
+}
